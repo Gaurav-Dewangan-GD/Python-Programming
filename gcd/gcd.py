83863eb2a89489e399255050d7b991e87bd676dd
Gaurav-Dewangan-GD <dewanganjeetu83@gmail.com>
2023-01-26 16:48:41 +0530

    gcd.py modified

--- gcd/gcd.py
+++ gcd/gcd.py
@@ -3,21 +3,21 @@ b = int(input("Enter the second number: "))
 
 # The list approach
 
-# def gcd(m,n):
-#     fm = []
-#     for i in range(1,m+1):
-#         if(m%i) == 0:
-#             fm.append(i)
-#     fn = []
-#     for j in range(1,n+1):
-#         if(n%j) == 0:
-#             fn.append(j)
+def gcd(m,n):
+    fm = []
+    for i in range(1,m+1):
+        if(m%i) == 0:
+            fm.append(i)
+    fn = []
+    for j in range(1,n+1):
+        if(n%j) == 0:
+            fn.append(j)
     
-#     cf = []
-#     for f in fm:
-#         if f in fn:
-#             cf.append(f)
-#     return(cf[-1])
+    cf = []
+    for f in fm:
+        if f in fn:
+            cf.append(f)
+    return(cf[-1])
 
 # The most recent common factor approach
 
